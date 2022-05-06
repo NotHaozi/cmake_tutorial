b686f75c8349c54ba89271d85f4e070a09f963d3
lab2080_haozi <lab2080_haozi@qq.com>
2022-05-06 17:10:34 +0800

    complete Step5

--- Help/guide/tutorial/Step5/MathFunctions/mysqrt.cxx
+++ Help/guide/tutorial/Step5/MathFunctions/mysqrt.cxx
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <cmath>
 #include "MathFunctions.h"
 
 // a hack square root calculation using simple operations
@@ -9,16 +9,22 @@ double mysqrt(double x)
     return 0;
   }
 
-  double result = x;
+  #if defined(HAVE_LOG) && defined(HAVE_EXP)
+    double result = exp(log(x) * 0.5);
+    std::cout << "Computing sqrt of " << x << " to be " << result
+         << " using log and exp" << std::endl;
+  #else
+    double result = x;
 
-  // do ten iterations
-  for (int i = 0; i < 10; ++i) {
-    if (result <= 0) {
-      result = 0.1;
+    // do ten iterations
+    for (int i = 0; i < 10; ++i) {
+      if (result <= 0) {
+        result = 0.1;
+      }
+      double delta = x - (result * result);
+      result = result + 0.5 * delta / result;
+      std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
     }
-    double delta = x - (result * result);
-    result = result + 0.5 * delta / result;
-    std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
-  }
+  #endif
   return result;
 }
